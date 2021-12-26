f0c7647f6a103fb6a9aafc487a9d88dfc01f9dd5
Owen <tamia123123@naver.com>
2021-12-26 17:52:04 +0900

    update

--- drum.js
+++ drum.js
@@ -8,8 +8,8 @@ window.addEventListener('keydown' , (e)=> {
 });
 
 window.addEventListener('keyup' , (e)=> {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+   
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio) return;
+    
     key.classList.remove('active');
 })
